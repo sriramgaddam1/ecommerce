804b1f9b285f868eed2eab0e6ae035685b900500
Sriram Gaddam <163174354+sriramgaddam1@users.noreply.github.com>
2026-02-08 00:36:22 +0530

    Update AdminOrderController.java

--- backend/src/main/java/com/example/ecom/controller/AdminOrderController.java
+++ backend/src/main/java/com/example/ecom/controller/AdminOrderController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin/orders")
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*", allowCredentials = "true") 
 public class AdminOrderController {
 
     private final OrderRepository orderRepository;
