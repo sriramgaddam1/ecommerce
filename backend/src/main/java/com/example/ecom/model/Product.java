d3d2ac1aa03e08ba2002fdb553ab2a84140d6ace
Sriram Gaddam <163174354+sriramgaddam1@users.noreply.github.com>
2026-02-08 13:45:12 +0530

    Update Product.java

--- backend/src/main/java/com/example/ecom/model/Product.java
+++ backend/src/main/java/com/example/ecom/model/Product.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.math.BigDecimal;
 import java.util.Date;
